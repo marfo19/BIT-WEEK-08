21362e06dc273870757c663fcb324e2da0abe882
DESKTOP-I7Q5440\Marko <markofoljan90@gmail.com>
2022-11-15 19:02:27 +0100

    added homework

---
+++ functions-exercises-07.js
@@ -0,0 +1,52 @@
+// 1. Write a function named tellFortune that:
+//  takes 4 arguments: number of children, partner&#39;s name, geographic location, job title.
+//  outputs your fortune to the screen like so: &quot;You will be a X in Y, and married to Z with N kids.&quot;
+// Call that function 3 times with 3 different values for the arguments.
+
+// 2. Write a function named calculateDogAge that:
+//  takes 1 argument: your puppy&#39;s age.
+//  calculates your dog&#39;s age based on the conversion rate of 1 human year to 7 dog years.
+//  outputs the result to the screen like so: &quot;Your doggie is NN years old in dog years!&quot;
+// Call the function three times with different sets of values.
+// Bonus: Add an additional argument to the function that takes the conversion rate of human to dog
+// years.
+
+// 3. Write a function named calculateSupply that:
+//  takes 2 arguments: age, amount per day.
+//  calculates the amount consumed for rest of the life (based on a constant max age).
+//  outputs the result to the screen like so: &quot;You will need NN to last you until the ripe old age of
+// X&quot;
+// Call that function three times, passing in different values each time.
+// Bonus: Accept floating point values for amount per day, and round the result to a round number.
+
+// 4. Create a function called celsiusToFahrenheit:
+//  Store a celsius temperature into a variable.
+//  Convert it to fahrenheit and output &quot;NN°C is NN°F&quot;.
+// Create a function called fahrenheitToCelsius:
+//  Now store a fahrenheit temperature into a variable.
+//  Convert it to celsius and output &quot;NN°F is NN°C.&quot;
+// NN is actual temperature you need to convert
+
+// 5. Create a function that validates a password to conform to the following rules:
+//  Length between 6 and 24 characters,
+//  At least one uppercase letter (A-Z).
+//  At least one lowercase letter (a-z).
+//  At least one digit (0-9).
+//  Maximum of 2 repeated characters (“aa” is OK, “aaa” is NOT).
+//  Supported special characters: ! @ # $ % ^ &amp; * ( ) + = _ - { } [ ] : ; &quot; &#39; ? &lt; &gt; , .
+//  Examples: validatePassword(&quot;P1zz@&quot;) ➞ false // Too short.
+//  validatePassword(&quot;iLoveYou&quot;) ➞ false // Missing a number.
+//  validatePassword(&quot;Fhg93@&quot;) ➞ true // OK!
+
+// 6. Create a function that finds how many prime numbers there are, up to the given integer.
+// Examples: primeNumbers(10) ➞ 4 // 2, 3, 5 and 7
+// primeNumbers(20) ➞ 8 // 2, 3, 5, 7, 11, 13, 17 and 19
+// primeNumbers(30) ➞ 10 // 2, 3, 5, 7, 11, 13, 17, 19, 23 and 29
+
+// 7. Create a function that returns an array that expands by 1 from 1 to the value of the input, and then
+// reduces back to 1. Items in the arrays will be the same as the length of the arrays.
+
+// Examples:
+// diamondArrays(1) ➞ [1],
+// diamondArrays(2) ➞ [1, 2, 2, 1]
+// diamondArrays(5) ➞ [1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 5, 4, 4, 4, 4, 3, 3, 3, 2, 2, 1]
